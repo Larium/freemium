ab43eb4da160e7290521226ae46bb899cfd9ab52
akDeveloper <andreas.kollaros@gmail.com>
2015-02-06 22:39:48 +0200

    Start adding tests

---
+++ test/Freemium/CouponRedemptionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+namespace Freemium;
+
+use DateTime;
+
+class CouponRedemptionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExpire()
+    {
+        $coupon = new Coupon();
+        $coupon->setProperties([
+            'description' => 'Discount coupon',
+            'discount_percentage' => 15
+        ]);
+
+        $subscription = new Subscription();
+
+        $couponRedemption = new CouponRedemption($coupon, $subscription);
+
+        $couponRedemption->expire();
+
+        $this->assertEquals(new DateTime('today'), $couponRedemption->getExpiredOn());
+    }
+
+    public function testActive()
+    {
+        $coupon = new Coupon();
+        $coupon->setProperties([
+            'description' => 'Discount coupon',
+            'discount_percentage' => 15
+        ]);
+
+        $subscription = new Subscription();
+
+        $couponRedemption = new CouponRedemption($coupon, $subscription);
+    }
+}
