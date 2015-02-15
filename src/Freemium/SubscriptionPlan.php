291db64bb450c640fda609ec33fe1ba29dcaa2b3
akDeveloper <andreas.kollaros@gmail.com>
2015-02-15 15:06:42 +0200

    add fields for Subscription plan

--- src/Freemium/SubscriptionPlan.php
+++ src/Freemium/SubscriptionPlan.php
@@ -6,5 +6,35 @@ namespace Freemium;
 
 class SubscriptionPlan extends AbstractEntity
 {
+    use Rate;
 
+    const YEARLY = 1;
+
+    const MONTHLY = 2;
+
+    protected $subscriptions = array();
+
+    /**
+     * How much this plan costs, in cents
+     *
+     * @var integer
+     * @access protected
+     */
+    protected $rate_cents;
+
+    /**
+     * Whether this plan cycles yearly or monthly
+     *
+     * @var integer
+     * @access protected
+     */
+    protected $cycle;
+
+    /**
+     * The name of plan
+     *
+     * @var string
+     * @access protected
+     */
+    protected $name;
 }
