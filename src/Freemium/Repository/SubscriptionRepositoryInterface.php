15034793998153950479bbc05f6102ec25028fd8
akDeveloper <andreas.kollaros@gmail.com>
2015-05-09 20:36:00 +0300

    add Subscription repository interface

---
+++ src/Freemium/Repository/SubscriptionRepositoryInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+namespace Freemium\Repository;
+
+class SubscriptionRepositoryInterface
+{
+    /**
+     * Return all subscriptions that must receive payment, meanining that
+     * paidThrough <= today and that their rate is > 0
+     *
+     * @access public
+     * @return array
+     */
+    public function findBillable();
+
+    /**
+     * Return all expired subscriptions.
+     *
+     * @access public
+     * @return array
+     */
+    public function findExpired();
+}
