932be65d7ef46a66bec679f1cefde1fb527d8872
akDeveloper <andreas.kollaros@gmail.com>
2015-08-15 22:41:57 +0300

    remove unused file test_init

--- test/test_init.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/User.php';
-require_once __DIR__ . '/Helper.php';
